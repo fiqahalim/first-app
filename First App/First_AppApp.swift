7457553eacf7bb48853c841e4316a9650eda8d08
syafiqah <syafiqah@tyrellsys.com.my>
2023-09-20 11:20:18 +0800

    Initial Commit

---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by TSMM034 on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
